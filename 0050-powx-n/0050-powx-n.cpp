d8ca8a0978246684ff5c800ee008a33fe98ce4a0
Deep-023 <100464710+Deep-023@users.noreply.github.com>
2023-06-23 15:02:37 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.46%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(n<0) {
-            n = abs(n);
-            x = 1/x;
-        }
+    double power(double x, int n){
+        int ans;
+        if(n==0)
+            return 1;
+        if(n==1)
+            return x;
+        
+        
         if(n&1){
-            return x*myPow(x, n-1);
+            x = x*power(x,n-1);
         }
-        else{
-            return myPow(x*x, n/2);
-        }
+        else
+            x = power(x*x,n/2);
+        
+        return x;
+    }
+    
+    double myPow(double x, int n) {
+        double ans = 0;
+        if(x==1)
+            return 1;
+        if(x==-1 && n==INT_MIN)
+            return 1;
+        
+        if(n==INT_MIN)
+            return 0;
+        if(n>=0)
+        ans = power(x,n);
+        else
+        ans = 1/power(x,abs(n));
+        return ans;
     }
 };
+
