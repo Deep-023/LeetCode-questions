20aa23a267699a035ef1f97b2c44297170aef372
Deep-023 <100464710+Deep-023@users.noreply.github.com>
2023-06-23 17:52:00 +0530

    Time: 8 ms (9.35%), Space: 7.3 MB (5.23%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    void rec(vector<string>& ans,unordered_map<int,string> m, string digits,int ind,string& answer){
+        
+        if (ind >= digits.size())
+            return ;
+        string h = m[digits[ind]-'0'];
+        for(int i=0;i<h.size();i++){
+            answer.push_back(h[i]);
+            rec(ans,m,digits,ind+1,answer);
+            if(answer.size()==digits.size())
+            ans.push_back(answer);
+            answer.pop_back();
+        }
+        return;
+        
+    }
+    
+    vector<string> letterCombinations(string digits) {
+        unordered_map<int,string> m;
+        vector<string> answer;
+        string ans;
+        m[2]="abc";
+        m[3]="def";
+        m[4]="ghi";
+        m[5]="jkl";
+        m[6]="mno";
+        m[7]="pqrs";
+        m[8]="tuv";
+        m[9]="wxyz";
+        string str;
+        rec(answer,m,digits,0,str);
+        return answer;
+        
+    }
+};
