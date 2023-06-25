0ce8a9114b72ba9076f1df6cfb9fe0a30652ce31
Deep-023 <100464710+Deep-023@users.noreply.github.com>
2023-06-25 11:49:35 +0530

    Time: 42 ms (49.91%), Space: 71.1 MB (76.52%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.cpp
@@ -72,3 +72,66 @@ public:
         return p;
     }
 };
+
+/*
+class Solution {
+private:
+    void reverse(ListNode * & head){
+        ListNode * curr = head;
+        ListNode * prev = nullptr;
+        ListNode * next = nullptr;
+        while(curr){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+    }
+    void insertHead(int num,ListNode * & ans){
+        ListNode * nodetoInsert = new  ListNode(num);
+        if(ans==nullptr){
+            ans = nodetoInsert;
+            return;
+        }
+        else{
+            nodetoInsert->next=ans;
+            ans=nodetoInsert;
+            return;
+        }
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        reverse(l1);
+        reverse(l2);
+        ListNode * ans = nullptr;
+        int carry=0;
+        while(l1 || l2){
+            if(l1 && l2){
+                int num = l1->val + l2->val + carry;
+                carry=num/10;
+                insertHead((num%10),ans);
+                l1=l1->next;
+                l2=l2->next;
+            }
+            if(l1 && !l2){
+                int num = l1->val + carry;
+                carry=num/10;
+                insertHead((num%10),ans);
+                l1=l1->next;
+            }
+            if(!l1 && l2){
+                int num = l2->val + carry;
+                carry=num/10;
+                insertHead((num%10),ans);
+                l2=l2->next;
+            }
+        }
+        
+        if(carry){
+            insertHead(carry,ans);
+        }
+        return ans;
+    }
+};
+*/
