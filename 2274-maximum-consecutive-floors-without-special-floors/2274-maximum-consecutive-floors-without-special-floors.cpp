8c1d14137ad0659728dd8c44f94fb72e8c8ffd0b
Deep-023 <100464710+Deep-023@users.noreply.github.com>
2023-07-09 17:15:58 +0530

    Time: 201 ms (35.63%), Space: 57.9 MB (55.11%) - LeetHub

--- 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.cpp
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.cpp
@@ -4,7 +4,9 @@ public:
         sort(special.begin(), special.end());
         int maxFloor = 0, n = special.size();
         for(int i = 0; i<n ;i++){
-            maxFloor = max(maxFloor, special[i]-bottom);
+            if(bottom != special[i]){
+                maxFloor = max(maxFloor, special[i]-bottom);
+            }
             bottom = special[i]+1;
         }
         maxFloor = max(maxFloor, top-special[n-1]);
