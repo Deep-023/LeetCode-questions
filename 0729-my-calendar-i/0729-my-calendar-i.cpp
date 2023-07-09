2de4df3ffcc271ff78b37240238cd7d20b6749e5
Deep-023 <100464710+Deep-023@users.noreply.github.com>
2023-07-09 23:47:53 +0530

    Time: 84 ms (96.80%), Space: 38.8 MB (35.25%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,4 +1,4 @@
-class MyCalendar {
+/*class MyCalendar {
 public:
     vector<int> st, ed;
     MyCalendar() {
@@ -21,9 +21,22 @@ public:
         return true;
     }
 };
-
+*/
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar* obj = new MyCalendar();
  * bool param_1 = obj->book(start,end);
  */
+class MyCalendar {
+    map<int, int> mp;   // <endTime, statIme>
+public:  
+    bool book(int start, int end) {
+        /*lookfor first endTIme that is larger than start time request. First condition checkes if the incoming request should be added to the list of booked slots.*/
+        auto itr = mp.upper_bound(start);
+        if(itr == mp.end() || end<= itr->second) {
+            mp[end]=start;
+            return true;
+        } 
+        else  return false;
+    }
+};
