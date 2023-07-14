5063a2807a9536e51d73f58fb0b7e6af5c314dcf
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-07-14 12:20:44 +0530

    Time: 6 ms (23.79%), Space: 8.6 MB (24.18%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int hIndex(vector<int>& c) {
+        sort(c.begin(),c.end());
+        unordered_map<int,int> m;
+        int ans = INT_MIN;
+        int n = c.size();
+        for(int i=0;i<n;i++){
+            if(!m.count(c[i]))
+                m[c[i]] = n-i;
+        }
+        for(auto i:m){
+            cout<<i.first<< " " <<i.second<<endl;
+            if(i.first <= i.second){
+                ans = max(ans,i.first);
+            }
+            else
+                ans= max(ans,i.second);
+        }
+        return ans;
+    }
+};
