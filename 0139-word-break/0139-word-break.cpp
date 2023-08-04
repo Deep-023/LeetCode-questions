08a6ed0c1963895513c95ffd1054b7e1397e5190
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-04 23:55:58 +0530

    Time: 21 ms (37.63%), Space: 14.9 MB (17.49%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,55 @@
+/*class Solution {
+public:
+    bool solve(string s, unordered_set<string> sett, int left, int idx, int n){
+        if(left==n)
+            return true;
+        if(idx >= n)
+            return false;
+        
+        string r = s.substr(left, idx-left+1);
+        bool ans1= false;
+        
+        if(sett.count(r)){
+            if(solve(s,sett,idx+1,idx+1,n)){
+                return true;
+            }
+        }
+        bool ans2 = solve(s,sett,left,idx+1,n);
+        
+        if(ans2){
+            return true;
+        }
+        return false;
+        
+    }
+    
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> sett(wordDict.begin(),wordDict.end());
+        int n= s.size();
+        return solve(s, sett, 0, 0,n);
+    }
+};*/
+
+class Solution {
+public:
+     unordered_map<string,bool>dp;
+    bool solve(string s,unordered_set<string>&m){
+        if(s.length()==0){
+            return true;
+        }
+        if(dp.find(s)!=dp.end())
+        return dp[s];
+        for(int i=0;i<s.length();i++){
+            string r=s.substr(0,i+1);
+            if(m.count(r)){
+                if(solve(s.substr(i+1),m))
+                return dp[s]=true;
+            }
+        }
+        return dp[s]=false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+         unordered_set<string>m(wordDict.begin(),wordDict.end());
+         return solve(s,m);
+    }
+};
