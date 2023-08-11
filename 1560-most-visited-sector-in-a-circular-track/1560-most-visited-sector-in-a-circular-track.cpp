d696a7419ee83b84423d26b141314834a4fa4d91
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-12 02:15:16 +0530

    Time: 7 ms (47.56%), Space: 11.9 MB (12.20%) - LeetHub

---
+++ 1560-most-visited-sector-in-a-circular-track/1560-most-visited-sector-in-a-circular-track.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> mostVisited(int n, vector<int>& rounds) {
+        unordered_map<int, int> m;
+        int n1 = rounds.size();
+        for (int i = 1; i < n1; i++) {
+            for (int j = rounds[i - 1]; j != rounds[i];) {
+                m[j]++;
+                if (j >= n)
+                    j = 1;
+                else
+                    j++;
+            }
+        }
+        m[rounds[n1 - 1]]++;
+
+        int mx = 0;
+        vector<int> ans;
+        
+        for (auto i : m)
+            mx = max(mx, i.second);
+
+        for (auto i : m) {
+            if (mx == i.second)
+                ans.push_back(i.first);
+        }
+        
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
