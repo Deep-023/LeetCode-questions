d705395d302db9849cf6a273130815eaa29d3718
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-13 12:32:03 +0530

    Time: 4 ms (65.01%), Space: 11.3 MB (34.42%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        int n = indices.size(), add=0;
+        int n = indices.size();
         int sSize = s.size();
         vector<int> visited(sSize,0);
         unordered_map<int,string> m;
         string ans;
+        
         for(int i=0;i<n;i++){
             bool temp = true;
             int sn = sources[i].size();
@@ -16,22 +17,21 @@ public:
                     break;
                 }
             }
-            //cout<<indices[i]+add<<" ";
             if(temp==true){
                 for(int j=0;j<sn;j++){
                     visited[indices[i]+j] = 1;
                  }
                 m[indices[i]]= targets[i];
-                //ans.replace(indices[i]+add,sn,targets[i]);
             }
-            //add+= targets[i].size()-sn;
         }
+        
         for(int i=0;i<sSize;i++){
             if(!visited[i])
                 ans.push_back(s[i]);
-            else if( !m[i].empty())
+            else if(!m[i].empty())
                 ans += m[i];
         }
+        
         return ans;
     }
 };
