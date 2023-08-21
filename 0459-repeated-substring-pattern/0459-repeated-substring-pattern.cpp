ec3f01be2cd0601665138d0a8a315bc6d25a0cdc
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-21 18:27:42 +0530

    Time: 57 ms (27.01%), Space: 91.9 MB (19.07%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -5,8 +5,9 @@ public:
         for (int i = 1; i <= n / 2; i++) {
             if (n % i == 0) {
                 string repeated = "";
+                string sb = s.substr(0, i);
                 for (int j = 0; j < n / i; j++) {
-                    repeated += s.substr(0, i);
+                    repeated += sb;
                 }
                 if (repeated == s) {
                     return true;
