6a8be6f505d8442b16f2bd8c63c1406e1a0c1bfd
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-27 12:18:56 +0530

    Time: 47 ms (95.57%), Space: 211.6 MB (28.33%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    bool solve(int k, int ind, vector<int>& stones, int n, int target, unordered_map<int,int>& m, int count, vector<vector<int>>&dp, int ik){
+    bool solve(int k, int ind, vector<int>& stones, int n, int target, unordered_map<int,int>& m, int count, vector<vector<int>>&dp){
         if(count==target)
             return true;
         if(count+k>target)
@@ -13,11 +13,11 @@ public:
         
         
         if(m.find(count+k)!=m.end()){
-            if(solve(k+1,m[count+k],stones,n,target,m,count+k,dp,k-1))
+            if(solve(k+1,m[count+k],stones,n,target,m,count+k,dp))
                 return dp[ind][k]=true;
-            else if(solve(k,m[count+k],stones,n,target,m,count+k,dp,k-1))
+            else if(solve(k,m[count+k],stones,n,target,m,count+k,dp))
                 return dp[ind][k]=true;
-            else if( (k-1!=0) && solve(k-1,m[count+k],stones,n,target,m,count+k,dp,k-1))
+            else if( (k-1!=0) && solve(k-1,m[count+k],stones,n,target,m,count+k,dp))
                 return dp[ind][k]=true;
         }
         
@@ -28,10 +28,10 @@ public:
     bool canCross(vector<int>& stones) {
         unordered_map<int,int> m;
         int n = stones.size();
-        vector<vector<int>>dp(n,vector<int>(10000,-1));
+        vector<vector<int>>dp(n,vector<int>(1000,-1));
         for(int i=0;i<n;i++){
             m[stones[i]]=i;
         }
-        return solve(1,1,stones,n,stones[n-1],m,0,dp,0);
+        return solve(1,1,stones,n,stones[n-1],m,0,dp);
     }
 };
