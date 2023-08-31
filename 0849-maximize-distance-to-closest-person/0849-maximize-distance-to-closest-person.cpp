df4646c56b2a20e19c1f44b56aaf9e5543dc592c
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-08-31 23:53:27 +0530

    Time: 17 ms (22.61%), Space: 17.5 MB (28.11%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cpp
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int maxDistToClosest(vector<int>& seats) {
         int n = seats.size();
-        vector<int> leftMax(n,INT_MAX);
-        vector<int> rightMax(n,INT_MAX);
+        vector<int> DisMax(n,INT_MAX);
         int count=INT_MAX;
         for(int i=0;i<n;i++){
             if(seats[i]==1)
                 count = 0;
             else if(count!=INT_MAX)
-                leftMax[i] = ++count;
+                DisMax[i] = ++count;
         }
         count=INT_MAX;
         for(int i=n-1;i>=0;i--){
             if(seats[i]==1)
                 count=0;
             else if(count!=INT_MAX)
-                rightMax[i] = ++count;
+                DisMax[i] = min(DisMax[i],++count);
         }
         int maxDistance=0;
         for(int i=0;i<n;i++){
-            if(seats[i] ==0 && maxDistance < min(leftMax[i],rightMax[i])){
-                maxDistance = min(leftMax[i],rightMax[i]);
+            if(seats[i] == 0 && maxDistance < DisMax[i]){
+                maxDistance = DisMax[i];
             }
         }
         return maxDistance;
