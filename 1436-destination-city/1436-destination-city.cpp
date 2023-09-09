b6463197713481ed0419fb41f34d6c2418ba4378
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-09-09 11:16:16 +0530

    Time: 12 ms (49.34%), Space: 11.8 MB (5.91%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string, vector<string>>m;
+        for(auto i:paths){
+            m[i[0]].push_back(i[1]);
+            m[i[1]];
+        }
+        
+        for(auto i:m){
+            if(i.second.size()==0)
+                return i.first;
+        }
+        return "";
+        
+    }
+};
