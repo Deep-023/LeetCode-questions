807bf36452a467af077202fe3e6d0d3d7166b876
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-09-09 11:54:18 +0530

    Time: 43 ms (68.71%), Space: 32.4 MB (53.06%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -8,18 +8,14 @@ public:
         }
         for(auto i:nums2){
             m2[i]=1;
-            m1[i]=0;
-        }
-        for(auto i:nums1){
-            m2[i]=0;
         }
         vector<vector<int>> ans(2);
         for(auto i:m1){
-            if(i.second==1)
+            if(i.second==1 && !m2.count(i.first))
                 ans[0].push_back(i.first);
         }
-        for(auto i:m2){
-            if(i.second==1)
+        for(auto i:m2 ){
+            if(i.second==1 && !m1.count(i.first))
                 ans[1].push_back(i.first);
         }
         return ans;
