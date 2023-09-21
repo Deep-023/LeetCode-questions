248cb0ed0a63ba584f891d9acdac2d984547ba69
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-09-21 23:24:24 +0530

    Time: 2 ms (54.27%), Space: 6.6 MB (5.13%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int countTime(string time) {
+        int ans = 1;
+        if(time[0]=='?'){
+            if(time[1]<'4' || time[1]=='?')
+                ans *= 3;
+            else
+                ans *= 2;
+        }
+        
+        if(time[1]=='?'){
+            if(time[0]=='?')
+                ans*=8;
+            else if(time[0]=='2')
+                ans *= 4;
+            else
+                ans*=10;
+        }
+        
+        if(time[3]=='?'){
+            ans *= 6;
+        }
+        
+        if(time[4]=='?')
+            ans*=10;
+        
+        return ans;
+    }
+};
