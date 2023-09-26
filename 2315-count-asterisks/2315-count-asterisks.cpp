096c4ce927d3c75e5a960ca495b1101522f9f354
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-09-26 13:37:21 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (14.63%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        int count=0;
-        int ans=0;
+        int ans=0,count=0;
         for(auto i:s){
             if(i == '|')
                 count++;
-            if(!(count&1)){
-                if(i =='*')
-                    ans++;
+            if(!(count&1) && i =='*'){
+                ans++;
             }
         }
         return ans;
