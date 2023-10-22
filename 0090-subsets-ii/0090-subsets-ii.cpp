37326d0430ebb30972e42889db693c6745361c4d
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-10-22 15:25:36 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (63.98%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    set<vector<int>> ans;
+    /*set<vector<int>> ans;
     
     void solve(vector<int>& nums,int idx, int n, vector<int>& output){
         
@@ -26,5 +26,20 @@ public:
             anss.push_back(str);
         }
         return anss;
+    }*/
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result = {{}};
+        int start;
+        for(int i=0; i<nums.size(); i++) {
+            if(i==0 || nums[i] != nums[i-1]) start = 0;
+            for(int end=result.size(); start<end; start++){
+                vector<int> clone = result[start];
+                clone.push_back(nums[i]);
+                result.push_back(clone);
+            }
+        }
+        return result;
     }
 };
