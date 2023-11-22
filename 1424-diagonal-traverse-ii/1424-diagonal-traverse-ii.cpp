b5bf9bd9f3736b161e8e832e47d60f8ab67afd6a
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-11-22 16:04:20 +0530

    Time: 130 ms (84.40%), Space: 66 MB (94.66%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
         vector<int> ans;
-        int mx =0;
+        /*int mx =0; //normal way
         unordered_map<int,vector<int>> map;
         for(int i=nums.size()-1;i>=0;i--){
             for(int j=0;j<nums[i].size();j++){
@@ -16,7 +16,25 @@ public:
             for(auto& j:map[i])
                 ans.push_back(j);
         }
+        */
         
+        //good algorithm using bfs, assume it like a binary tree [0,0] will have [1,0] & [0,1] as left and right child and 
+        //same for rest of nodes, if we do bfs in this we will get the answer, but all character will appear 2 times as somebody
+        //right node can be somebody left node so we will only consider left node of 0th column
+        int n = nums.size();
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        while(!q.empty()){
+            int i = q.front().first, j=q.front().second;
+            q.pop();
+            ans.push_back(nums[i][j]);
+            
+            if(j==0 && i+1<n)
+                q.push({i+1,j});
+            if(i<n && j+1<nums[i].size())
+                q.push({i,j+1});
+            
+        }
             
         return ans;
     }
