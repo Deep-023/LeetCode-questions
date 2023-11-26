fc98b4d08697cea150bee9a20db412ea12e50080
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-11-27 00:20:01 +0530

    Time: 150 ms (77.84%), Space: 75.3 MB (49.43%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -1,67 +1,27 @@
 class Solution {
 public:
-    
-    vector<int> leftMin(vector<int>& arr, int n){
-        stack<int> s;
-        vector<int>ans(n,-1);
-        s.push(0);
-        for(int i=1;i<n;i++){
-            while(!s.empty() && arr[s.top()] >= arr[i])
-                s.pop();
-            
-            if(!s.empty())
-                ans[i] = s.top();
-            s.push(i);
-        }
-        return ans;
-    }
-    
-    vector<int> rightMin(vector<int>& arr, int n){
-        stack<int> s;
-        vector<int>ans(n,n);
-        s.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(!s.empty() && arr[s.top()] >= arr[i])
-                s.pop();
-            
-            if(!s.empty())
-                ans[i] = s.top();
-            s.push(i);
-        }
-        return ans;
-    }
-    
-    int calLargest(vector<int>& histo, int n){
-        
-        vector<int> left = leftMin(histo,n);
-        vector<int> right = rightMin(histo,n);
-        
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            int num = histo[i];
-            ans = max(ans, num*(right[i]-left[i]-1));
-        }
-        return ans;
-    }
-    
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> histo(m,0);
+        int m = matrix.size(), n = matrix[0].size();
         int ans = 0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]!=0)
-                    histo[j] += matrix[i][j];
-                else
-                    histo[j] = 0;
+        vector<int> height(n, 0);
+		
+		// view each row and its above as pillars 
+        for(int i = 0; i < m; ++i){
+			// calculate heights
+            for(int j = 0; j < n; ++j){
+                if(matrix[i][j] == 0) height[j] = 0;
+                else height[j] += 1;
             }
-            vector<int> dummy = histo;
-            sort(dummy.begin(),dummy.end());
-            ans = max(ans,calLargest(dummy,m));
+			
+			// sort pillars
+            vector<int> order_height = height;
+            sort(order_height.begin(), order_height.end());
+			
+			// iterate to get the maxium rectangle
+            for(int j = 0; j < n; ++j){
+                ans = max(ans, order_height[j] * (n - j));
+            }
         }
-        
         return ans;
     }
 };
