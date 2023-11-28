7ee7b5c4abdf6fdd3023e7a61efd4da386cbe9cf
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-11-28 23:46:01 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (81.50%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -12,16 +12,38 @@
 class Solution {
 public:
     
-    void solve(TreeNode* root, vector<int>& ans){
+    /*void solve(TreeNode* root, vector<int>& ans){
         if(root==NULL)
             return;
         solve(root->left, ans);
         ans.push_back(root->val);
         solve(root->right, ans);
-    }
+    }*/
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        solve(root,ans);
+        //solve(root,ans);
+        //lets do it using morris traversal
+        
+        TreeNode* current = root;
+        while(current != NULL){
+            if(current->left==NULL){
+                ans.push_back(current->val);
+                current = current->right;
+            }
+            else{
+                TreeNode* pre = current->left;
+                while(pre->right != NULL && pre->right != current)
+                    pre = pre->right;
+                if(pre->right==NULL){
+                    pre->right = current;
+                    current = current->left;
+                }else{
+                    pre->right=NULL;
+                    ans.push_back(current->val);
+                    current = current->right;
+                }
+            }
+        }
         return ans;
     }
 };
