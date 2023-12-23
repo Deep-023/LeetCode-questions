bb262c02b9eb05b5cb5a6f5beb87812b772f38cc
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2023-12-23 15:51:42 +0530

    Time: 5 ms (19.89%), Space: 7.1 MB (93.92%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,8 +1,17 @@
 class Solution {
+    
+    struct PairHash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1, T2>& p) const {
+        // Implement a hash function for pair<int, int>
+        return std::hash<T1>{}(p.first) ^ std::hash<T2>{}(p.second);
+    }
+};
+  
 public:
     bool isPathCrossing(string path) {
         pair<int,int> origin;
-        set<pair<int,int>> set;
+        unordered_set<pair<int,int>, PairHash> set;
         origin = {0,0};
         set.insert({0,0});
         for(int i=0;i<path.size();i++){
