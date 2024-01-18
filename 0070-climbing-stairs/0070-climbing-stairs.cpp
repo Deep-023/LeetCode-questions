ee2999d7dd46ad174638cc6f553f17a6eed2c7f9
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-01-18 08:51:34 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n) {
 
-        int dp[46] = {1};
+        int dp[46] = {0};
         dp[0]=1;
         for(int i=0;i<=n;i++){
             if(i+1 <= n)
