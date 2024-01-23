892a6f6e1c668e718a25dfe4052b3ea4d3e401bd
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-01-23 23:59:14 +0530

    Time: 3 ms (93.80%), Space: 9.6 MB (82.42%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void solve(vector<string>& arr, vector<int> count, int n , int idx, int total, int& ans){
+    void solve(vector<string>& arr, vector<int>& count, int n , int idx, int total, int& ans){
         
         if(idx>=n){
             ans = max(ans,total);
@@ -15,11 +15,18 @@ public:
             count[arr[idx][i] - 'a']++;
             if(count[arr[idx][i] - 'a'] > 1)
             {
+                for(int j=i;j>=0;j--){
+                    count[arr[idx][j] - 'a']--;
+                }
                 return;
             }
         }
         
         solve(arr,count,n,idx+1,n1+total,ans);
+        
+        for(int i=0;i<n1;i++){
+            count[arr[idx][i] - 'a']--;
+        }
     }
     
     int maxLength(vector<string>& arr) {
