8e432d1e739517bce9e5b44a4180dfbd5827b679
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-01-27 17:43:14 +0530

    Time: 10 ms (76.37%), Space: 7.7 MB (89.01%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    int solve(int n, int k, vector<vector<int>>& dp){
+    /*int solve(int n, int k, vector<vector<int>>& dp){
         
         if( k == 0 ){
             return 1;
@@ -20,13 +20,28 @@ public:
         }
         
         dp[n][k] = ans%((int)(1e9+7));
-        return dp[n][k];
-        
-        
-    }
+        return dp[n][k];   
+    }*/
     
     int kInversePairs(int n, int k) {
-        vector<vector<int>>dp (n+1,vector<int>(k+1,-1));
-        return solve(n,k,dp);
+        /*vector<vector<int>>dp (n+1,vector<int>(k+1,-1));
+        return solve(n,k,dp);*/
+        
+        int mod = 1e9+7;
+        vector<long long> curr(k+1,0),last(k+1,0);
+        last[0] = 1;
+        curr[0] = 1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=k;j++){
+                curr[j] = (curr[j-1]+last[j])%mod;
+                if(j>=i)
+                    curr[j] = (curr[j]-last[j-i] + mod)%mod;
+            }
+            last = curr;
+        }
+        
+        
+        return curr[k];
     }
 };
