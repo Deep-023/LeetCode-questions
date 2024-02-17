9bd3c7da2a9f45c7db72c6f423c6f5189ae45848
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-02-17 15:45:18 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (36.05%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {
-        int a = start, b = goal;
+    int minBitFlips(int a, int b) {
         int ans = 0;
         while(a>0 || b>0){
             if((a&1)!=(b&1)){
