b20e31d07dc873cc820cc11f61724f7b929ad59a
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-03-02 10:27:46 +0530

    Time: 44 ms (57.87%), Space: 164.8 MB (22.67%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    string ans = "";
-    void expand(string &s , int left ,int right)
-    {
-        while(left >= 0 &&  right < s.size())
-        {
-            if(s[left] != s[right])
-                break;
-            left--,right++;
+    int helper(string s, int i, int j) {
+  
+        while (i >= 0 && j < s.length() && s[i] == s[j]) {
+            i--;
+            j++;
+          
         }
-        if(ans.size() < right - left )
-            ans = s.substr(left + 1 , right - left - 1);
+        return j - i-1;
+      
     }
+
     string longestPalindrome(string s) {
-        for(int i = 0 ; i < s.size() ; i++)
-        {
-            expand(s , i , i);
-            expand(s , i , i+1);
+        int start = 0, end = 0, len = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = helper(s, i, i + 1);
+            int len2 = helper(s, i, i);
+
+            len = max(len,max(len1, len2));
+
+            if (len > (end - start)+1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
         }
-        return ans;
+        return s.substr(start, len);
     }
 };
