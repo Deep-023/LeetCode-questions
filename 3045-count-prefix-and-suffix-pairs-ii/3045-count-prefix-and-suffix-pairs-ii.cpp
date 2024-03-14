d7ec1f5854b5d8f349c36be310bbf29b211f72ad
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-03-15 01:18:59 +0530

    Time: 617 ms (45.95%), Space: 181.4 MB (55.38%) - LeetHub

---
+++ 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    
+    vector<int> zIndex(string& str){
+        int n = str.size();
+        vector<int>z(n);
+        z[0]=n;
+        for(int i=1,l=0,r=0;i<n;i++){
+            if(i<=r)
+                z[i] = min(z[i-l],r-i+1);
+            for(int& j=z[i];i+j<n && str[j]==str[i+j];j++);
+            if(z[i]>r-i+1){
+                l=i;
+                r = z[i]+i-1;
+            }   
+        }
+        return z;
+    }
+    
+    
+    long long countPrefixSuffixPairs(vector<string>& words) {
+        
+        unordered_map<int64_t,int> hash;
+        long long mod = 1e17+3;
+        long long ans = 0;
+        
+        for(int i=0;i<words.size();i++){
+            vector<int> z = zIndex(words[i]);
+            int64_t val = 0;
+            int n = words[i].size();
+            for(int j=1;j<=n;j++){
+                val = ((27LL*val)%mod + (1LL + words[i][j-1]-'a'))%mod;
+                if(z[n-j]==j){
+                    ans += hash[val];
+                }
+            }
+            hash[val]++;
+        }
+        
+        return ans;
+    }
+};
