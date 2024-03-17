cf2896b1829a44ef595617044031cbb455484389
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-03-17 13:08:18 +0530

    Time: 15 ms (31.27%), Space: 22 MB (5.24%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,50 +2,29 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
-        vector<vector<int>> ans;
-        stack<vector<int>> st;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(),intervals.end());
+        stack<vector<int>>st;
+        st.push(intervals[0]);
         int n = intervals.size();
-        bool flag = true;
         
-        if(n==0)
-            return {newInterval};
-        
-        for(int i=0;i<n;i++){
-            if(flag && newInterval[0]<=intervals[i][0]){
-                if(st.empty() || (st.top()[1]<newInterval[0])){
-                    st.push(newInterval);
-                }else{
-                    vector<int> tp = st.top();
-                    st.pop();
-                    st.push({min(tp[0],newInterval[0]),max(tp[1],newInterval[1])});
-                }
-                flag = false;
-            }
-                if(st.empty() || (st.top()[1]<intervals[i][0])){
-                    st.push(intervals[i]);
-                }else{
-                    vector<int> tp = st.top();
-                    st.pop();
-                    st.push({min(tp[0],intervals[i][0]),max(tp[1],intervals[i][1])});
-                }
-        }
-        
-        if(flag){
-            if(st.top()[1]>=newInterval[0]){
+        for(int i=1;i<n;i++){
+            if(!st.empty() && st.top()[1] >= intervals[i][0]){
                 vector<int> tp = st.top();
                 st.pop();
-                st.push({min(tp[0],newInterval[0]),max(tp[1],newInterval[1])});
+                st.push({tp[0],max(tp[1],intervals[i][1])});
+            }else{
+                st.push(intervals[i]);
             }
-            else
-                st.push(newInterval);
-        }
+        }      
         
+        intervals.clear();
         while(!st.empty()){
-            ans.push_back(st.top());
+            intervals.push_back(st.top());
             st.pop();
         }
         
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(intervals.begin(),intervals.end());
+        return intervals;
     }
 };
