fa32069d117e1f26f9a55b9b16091acd23cb9a68
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-03-17 13:11:24 +0530

    Time: 11 ms (65.36%), Space: 21.4 MB (6.58%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -4,27 +4,20 @@ public:
         
         intervals.push_back(newInterval);
         sort(intervals.begin(),intervals.end());
-        stack<vector<int>>st;
-        st.push(intervals[0]);
+        vector<vector<int>>st;
+        st.push_back(intervals[0]);
         int n = intervals.size();
         
         for(int i=1;i<n;i++){
-            if(!st.empty() && st.top()[1] >= intervals[i][0]){
-                vector<int> tp = st.top();
-                st.pop();
-                st.push({tp[0],max(tp[1],intervals[i][1])});
+            if(!st.empty() && st.back()[1] >= intervals[i][0]){
+                vector<int> tp = st.back();
+                st.pop_back();
+                st.push_back({tp[0],max(tp[1],intervals[i][1])});
             }else{
-                st.push(intervals[i]);
+                st.push_back(intervals[i]);
             }
         }      
-        
-        intervals.clear();
-        while(!st.empty()){
-            intervals.push_back(st.top());
-            st.pop();
-        }
-        
-        reverse(intervals.begin(),intervals.end());
-        return intervals;
+
+        return st;
     }
 };
