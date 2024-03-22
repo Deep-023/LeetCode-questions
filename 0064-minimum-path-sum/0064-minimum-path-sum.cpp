c8d8ad3f482eb59b1980001bbc4ef10eaa448537
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-03-23 00:28:47 +0530

    Time: 3 ms (95.27%), Space: 12.6 MB (39.02%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, int i, int j,int m, int n,  vector<vector<int>>& dp){
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
         
-        if(i==m-1 && j==n-1){
-            return grid[i][j];
+        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
+        dp[0][0] = grid[0][0];
+        for(int i=1;i<m;i++){
+            dp[0][i] = dp[0][i-1]+grid[0][i];
         }
         
-        if(i>=m || j>=n)
-            return 500;
+        for(int i=1;i<n;i++)
+            dp[i][0] = dp[i-1][0]+grid[i][0];
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){    
+                dp[i][j] = grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
         
-        int count = solve(grid,i+1,j,m,n,dp)+grid[i][j];
-        count = min(count, solve(grid,i,j+1,m,n,dp) +grid[i][j]);
-        return dp[i][j] = count;
-    }
-    
-    int minPathSum(vector<vector<int>>& grid) {
-        int n=grid[0].size(), m=grid.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(grid,0,0,m,n,dp);
+        return dp[n-1][m-1];
+        
     }
 };
