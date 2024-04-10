0889966bcd353b4d304a00141d5e846e09463fb9
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-11 00:01:32 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (91.52%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -2,33 +2,23 @@ class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         sort(deck.begin(),deck.end());
-        deque<int> dq;
+        queue<int> dq;
         int n = deck.size();
         for(int i=0;i<n;i++){
-            dq.push_back(i);
+            dq.push(i);
         }
         
         vector<int> res(n);
         
         for(auto &i : deck){
             int idx = dq.front();
-            dq.pop_front();
+            dq.pop();
             res[idx] = i;
             if(!dq.empty()){
-                dq.push_back(dq.front());
-                dq.pop_front();
+                dq.push(dq.front());
+                dq.pop();
             }
         }
         return res;
     }
 };
-
-// 17 13 11 7 5 3 2
-
-//17
-//13 17 
-//11 17 13
-//7 13 11 17
-//5 17 7 13 11
-//3 11 5 17 7 13
-//2 13 3 11 5 17 7
