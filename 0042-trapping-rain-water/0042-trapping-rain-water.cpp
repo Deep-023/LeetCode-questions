7af29a1620c9fa50cf4db9c53a3d8b449f072b70
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-12 11:31:16 +0530

    Time: 13 ms (35.99%), Space: 23.2 MB (25.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
     int trap(vector<int>& height) {
-        int sum=0;
-        int n= height.size();
-        vector<int> leftMax(n), rightMax(n);
-        leftMax[0]=0; rightMax[n-1]=0;
-        
-        for(int i=1;i<n;i++)
-            leftMax[i] = max(leftMax[i-1],height[i-1]);
-        
-        for(int i=n-2;i>=0;i--)
-            rightMax[i] = max(rightMax[i+1],height[i+1]);
-        
+        int n = height.size();
+        vector<int> right(n);
+        vector<int> left(n);
+    
         for(int i=0;i<n;i++){
-            int level = min(leftMax[i],rightMax[i]);
-            if(level>height[i])
-                sum+= level-height[i];
+            left[i] = i==0 ? height[i] : max(left[i-1],height[i]);
+        }
+        for(int i=n-1;i>=0;i--){
+            right[i] = i==n-1 ? height[i] : max(right[i+1],height[i]);
         }
         
-        return sum;
-            
+        int ans = 0;
+        for(int i=0;i<n;i++)
+            ans+= (min(left[i],right[i])-height[i]);
+           // cout<<left[i]<<" "<<right[i]<<endl;
+        
+        
+        return ans;
     }
 };
