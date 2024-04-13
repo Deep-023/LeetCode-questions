171afff5c5cff4c96138bdd554a49a92a1025f0e
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-13 14:00:11 +0530

    Time: 35 ms (46.62%), Space: 19.5 MB (46.35%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,6 +1,53 @@
 class Solution {
 public:
+    
+    int largestHisto(vector<int>& mat){
+        int n = mat.size();
+        vector<int> right(n); vector<int> left(n);
+        stack<int> st;
+        st.push(-1);
+        
+        for(int i=n-1;i>=0;i--){
+            while(st.top() != -1 && mat[i]<=mat[st.top()])
+                st.pop();
+            right[i] = st.top();
+            st.push(i);
+        }
+        
+        while(st.top() != -1)
+                st.pop();
+        
+        for(int i=0;i<n;i++){
+            while(st.top() != -1 && mat[i]<=mat[st.top()])
+                st.pop();
+            left[i] = st.top();
+            st.push(i);
+        }
+        
+        int ans = 0;
+        
+        for(int i=0;i<n;i++){
+            if(right[i]==-1) right[i]=n;
+            ans = max(ans,mat[i]*(right[i]-left[i]-1));
+        }
+        
+        return ans;
+    }
+    
     int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size(),m=matrix[0].size();
+        vector<int> mat(m,0);
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mat[j] = matrix[i][j]=='0' ? 0 : mat[j]+1;    
+            }
+            ans = max(ans,largestHisto(mat));
+        }
+        return ans;
+    }
+    
+    /*int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size(), m=matrix[0].size();
         vector<vector<int>> preSum(n,vector<int>(m,0));
         
@@ -28,5 +75,5 @@ public:
         return ans;
         
         
-    }
+    }*/
 };
