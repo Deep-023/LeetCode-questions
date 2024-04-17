980d76df5f0fc943e0e27be45e30365ba517225b
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-17 16:02:36 +0530

    Time: 6 ms (65.56%), Space: 10.5 MB (65.29%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,7 +3,7 @@ public:
     int maxSatisfaction(vector<int>& satis) {
         sort(satis.begin(),satis.end());
         int n = satis.size();
-        vector<int> dp(n+1,0);
+        /*vector<int> dp(n+1,0);
         
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<=i;j++){
@@ -12,8 +12,25 @@ public:
                 
                 dp[j] = max(incl,excl);
             }
+        }*/
+        
+        int ans = 0;
+        int k=1,cut=-1;
+        vector<int> sum(n,0);
+        for(int i=0;i<n;i++){
+            if(satis[i]>0)
+                ans = ans+(satis[i]*k++);
+            else
+                cut=i;
+            sum[n-1-i] = satis[n-1-i] + (i==0 ? 0 : sum[n-i]);
+        }
+        int mxans = ans;
+        for(int i=cut;i>=0;i--)
+        {
+            ans += sum[i];
+            mxans = max(ans,mxans);
         }
         
-        return dp[0];
+        return mxans;
     }
 };
