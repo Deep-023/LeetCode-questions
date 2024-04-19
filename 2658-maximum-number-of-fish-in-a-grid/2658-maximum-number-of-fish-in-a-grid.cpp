f722a2d3e4f24ce5ecfe73314bff9cd621a98974
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-19 11:32:45 +0530

    Time: 53 ms (58.52%), Space: 93 MB (92.38%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     
-    int dfs(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& visited){
+    int dfs(vector<vector<int>>& grid, int i, int j){
         
-        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || visited[i][j]==1 || grid[i][j] == 0)
+        if(i>=grid.size() || j>=grid[0].size() || i<0 || j<0 || grid[i][j] == 0)
             return 0;
         
         int ans = grid[i][j];
+        grid[i][j] = 0;
         
-        visited[i][j] = 1;
+        ans += dfs(grid,i+1,j);
+        ans += dfs(grid,i,j+1);
+        ans += dfs(grid,i-1,j);
+        ans += dfs(grid,i,j-1);
         
-        ans += dfs(grid,i+1,j,visited);
-        ans += dfs(grid,i,j+1,visited);
-        ans += dfs(grid,i-1,j,visited);
-        ans += dfs(grid,i,j-1,visited);
-        
-       // cout<<ans<<endl;
         return ans;
         
     }
     
     int findMaxFish(vector<vector<int>>& grid) {
         int n = grid.size(),m=grid[0].size(),ans=0;
-        vector<vector<int>> visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] != 0){
-                    ans = max(ans,dfs(grid,i,j,visited));
-                    visited = vector<vector<int>>(n,vector<int>(m,0));
-                    //cout<<"________________________"<<endl;
+                    ans = max(ans,dfs(grid,i,j));
                 }
-                
             }
         }
         
