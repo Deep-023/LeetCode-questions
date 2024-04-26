afed758dd1684a75eefe14720d0496737b3b4190
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-04-26 09:53:09 +0530

    Time: 334 ms (29.24%), Space: 18.3 MB (34.89%) - LeetHub

--- 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/1224-minimum-falling-path-sum-ii.cpp
@@ -1,6 +1,7 @@
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid, int i, int m, int prev, vector<vector<int>>&dp){
+    /*int solve(vector<vector<int>>& grid, int i, int m, int prev, vector<vector<int>>&dp){
         if(i>=grid.size())
             return 0;
         
@@ -16,10 +17,32 @@ public:
         }
         
         return dp[i][prev+1]=ans;
-    }
+    }*/
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size(),m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m+1,-1));
-        return solve(grid,0,m,-1,dp);
+        //vector<vector<int>>dp(n,vector<int>(m+1,-1));
+        //return solve(grid,0,m,-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+2,INT_MAX));
+        for(int i=0;i<m+2;i++) dp[n][i] = 0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=-1;j--){
+                for(int k=0;k<m;k++){
+                    if(j != k){
+                        dp[i][j+1] = min(dp[i][j+1],grid[i][k]+dp[i+1][k+1]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][0];
+
     }
 };
+
+auto init = [](){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
