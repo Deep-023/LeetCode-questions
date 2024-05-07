6ad061a182d0f88025ed47e3b32bfbd6aabcbe14
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-05-07 20:32:39 +0530

    Time: 47 ms (15.5%), Space: 9.7 MB (91.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,26 @@
 class Solution {
- public:
-  int minDistance(string word1, string word2) {
-    const int m = word1.length();
-    const int n = word2.length();
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+public:
+    int dp[502][502];
+    int minDistance(string word1, string word2) {
+        memset(dp,0,sizeof(dp));
+        int n = word1.size(),m=word2.size();
+        dp[n][m] = 0;
+        for(int i=0;i<m;i++)
+            dp[n][i] = m-i; 
+        for(int i=0;i<n;i++)
+            dp[i][m] = n-i; 
 
-    for (int i = 1; i <= m; ++i)
-      dp[i][0] = i;
-
-    for (int j = 1; j <= n; ++j)
-      dp[0][j] = j;
-
-    for (int i = 1; i <= m; ++i)
-      for (int j = 1; j <= n; ++j)
-        if (word1[i - 1] == word2[j - 1])//same characters
-          dp[i][j] = dp[i - 1][j - 1];//no operation
-        else
-          dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[m][n];
-  }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(word1[i]==word2[j])
+                    dp[i][j] = dp[i+1][j+1];
+                else{
+                    int temp =INT_MAX;
+                    temp = min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
+                    dp[i][j]= temp+1;
+                }
+            }
+        }
+        return dp[0][0];
+    }
 };
