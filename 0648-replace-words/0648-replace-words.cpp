ab684777486de985549e44a30eb3c00498ca9306
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-06-07 12:26:38 +0530

    Time: 72 ms (81.51%), Space: 73.8 MB (66.63%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    class Trie{
+        public:
+        Trie* arr[26];
+        bool isTerminal;
+
+        Trie(){
+            for(int i=0;i<26;i++) arr[i] = NULL;
+            isTerminal = false;
+        }
+    };
+
+    Trie* insert(vector<string>& dict){
+        Trie* trie = new Trie();
+        for(int i=0;i<dict.size();i++){
+            Trie* temp = trie;
+            for(int j=0;j<dict[i].size();j++){
+                int ch = dict[i][j]-'a';
+                if(temp->arr[ch] == NULL)
+                    temp->arr[ch] = new Trie();
+                temp = temp->arr[ch];
+                if(j==dict[i].size()-1)
+                    temp->isTerminal = true;
+            }
+        }
+        return trie;
+    }
+
+    string check(string word, Trie* root){
+        int count = 0,flag=1;
+        Trie* temp = root;
+        for(int i=0;i<word.size();i++){
+            int ch = word[i]-'a';
+            if(temp->arr[ch]==NULL) break;
+            temp = temp->arr[ch];
+            count++;
+            if(temp->isTerminal) {flag = 0 ; break;}
+        }
+
+        return flag ? word : word.substr(0,count);
+    }
+    
+
+    string replaceWords(vector<string>& dict, string sen) {
+        Trie* root = insert(dict);
+        string ans;
+
+        stringstream s(sen);
+        string word;
+
+        while(getline(s,word,' ')){
+            if(ans.size()>0) ans+=" ";
+            ans += check(word,root);
+        }
+        return ans;
+    }
+};
