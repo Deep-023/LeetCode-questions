3abdd9602eef28ad698413a5659ff1916eb573f8
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-06-09 16:30:14 +0530

    Time: 140 ms (77.44%), Space: 40.6 MB (54.89%) - LeetHub

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        auto comp = [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        };
+        sort(points.begin(), points.end(), comp);
+        int ans = 0;
+        for(int i = 0; i < points.size(); ++i){
+            int cx = points[i][0], cy = points[i][1];
+            int k = -1e9-5;
+            for(int j = i+1; j < points.size(); ++j){
+                int tx = points[j][0], ty = points[j][1];
+                if(ty>cy) continue;
+                if(ty>k){
+                    k = ty;
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
