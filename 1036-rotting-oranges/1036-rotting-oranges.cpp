e76187c491d6d5e000f545d6f6ba429c3745eb19
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2024-06-10 08:38:03 +0530

    Time: 11 ms (12.33%), Space: 15.6 MB (95.03%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    using pp = tuple<int,int,int>;
+    int dir[4][2] = {{0,1},{1,0},{-1,0},{0,-1}}; 
+    bool isSafe(int i, int j, int n, int m){
+        return i>=0 && j>=0 && i<n && j<m;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int ans = 0,n=grid.size(),m=grid[0].size();
+        queue<pp> q;
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(grid[i][j]==2) q.push({i,j,0});
+
+        while(!q.empty()){
+            auto [i,j,cost] = q.front();
+            q.pop(); 
+            for(int k=0;k<4;k++){
+                int newi = i+dir[k][0];
+                int newj = j+dir[k][1];
+
+                if(isSafe(newi,newj,n,m) && grid[newi][newj] == 1){
+                    grid[newi][newj]=2;
+                    q.push({newi,newj,cost+1});
+                }
+            }
+            ans = max(ans,cost);
+        }
+
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(grid[i][j]==1) return -1;
+
+        return ans;   
+    }
+};
