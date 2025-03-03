95c8ad27624fbdccf1d6d1b81787663ff99c1294
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2025-03-03 23:34:39 +0530

    Time: 56 ms (53.28%), Space: 24.6 MB (53.57%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
-    int n,m;
-    int dir[4][2] = {{0,1},{1,0},{0,-1},{-1,0}};
-    bool isSafe(int i, int j){
-        return i>=0 && j>=0 && i<n && j<m;
-    }
-    bool dfs(vector<vector<int>> &heights, int i, int j, int& diff, vector<vector<bool>> &vis){
-        if(i==n-1 && j==m-1)
-            return true;
-        
-        vis[i][j] = 1;
-        
-        for(int k=0;k<4;k++){
-            int newi = i+dir[k][0];
-            int newj = j+dir[k][1];
-            
-            if(isSafe(newi,newj) && vis[newi][newj]==0){
-                if((abs(heights[i][j] - heights[newi][newj]) <= diff) && dfs(heights,newi,newj,diff,vis)){
-                    vis[i][j] = 0;
-                    return true;
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>> visit(heights.size(), vector<int>(heights[0].size(), INT_MAX));
+        pq.push({0,{0,0}});
+        vector<int> dirs{0,1,0,-1,0};
+        visit[0][0]=0;
+        int ans=0,res=INT_MAX;
+        while(!pq.empty()) {
+            int val = pq.top().first, x=pq.top().second.first, y=pq.top().second.second;
+            ans=max(ans, val);
+            pq.pop();
+            if(x==heights.size()-1 && y==heights[0].size()-1) break;
+            for(int i=0;i<4;i++) {
+                int nx=x+dirs[i], ny=y+dirs[i+1];
+                if(nx>=0 && nx<heights.size() && ny>=0 && ny<heights[0].size()){
+                    int currval = max(val,abs(heights[nx][ny] - heights[x][y]));
+                    if(visit[nx][ny]>currval) {
+                    pq.push({currval, {nx, ny}});
+                    visit[nx][ny]=currval;
                 }
             }
-        }
-        return false;
-    }
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int low = 0, high = 1e7;
-        n = heights.size(); m=heights[0].size();
-        int ans;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            vector<vector<bool>> vis(n,vector<bool>(m));
-            if(dfs(heights,0,0,mid,vis)==true){
-                ans = mid;
-                if(low==high) break;
-                high = mid-1;
-            }else{
-                if(low==high) break;
-                low = mid+1;
             }
         }
-        
         return ans;
     }
 };
