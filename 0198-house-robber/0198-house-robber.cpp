a876e91d95a5dd60d812e5f2e9b9e140e826af8f
Deepanshu Aggarwal <100464710+Deep-023@users.noreply.github.com>
2025-04-27 00:23:44 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,13 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, int n, int idx, bool take,vector<vector<int>>& dp){
-        
-        if(idx>=n)
-            return 0;
-        
-        if(dp[idx][take] != -1)
-            return dp[idx][take];
-        
-        int ans = INT_MIN;
-        
-        
-        if(take){
-            ans = max(ans, arr[idx]+solve(arr,n,idx+1,false,dp));
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        if(n>1) dp[1] = max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
         }
-        
-        ans = max(ans, solve(arr,n,idx+1,true,dp));
-        
-        return dp[idx][take]=ans;
-        
-    }
-
-    int rob(vector<int>& arr) {
-        int n= arr.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(arr,n,0,true,dp);
+        return dp[n-1];
     }
 };
